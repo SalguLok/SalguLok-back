feadd13a76bb429dfa8970811fa5049389956c3b
jettieb <bokjihee@naver.com>
2025-07-26 17:09:08 +0900

    Feat: 에러 핸들러 구현

---
+++ salgulok/src/main/java/com/salgulok/global/exception/ErrorCode.java
@@ -0,0 +1,20 @@
+package com.salgulok.global.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode {
+    // Default
+    ERROR(400, "요청 처리에 실패했습니다."),
+    UNAUTHORIZED_ACCESS(401, "인증되지 않은 사용자입니다."),
+
+    // auth
+    KAKAO_LOGIN_ERROR(401, "카카오 로그인 인증에 실패했습니다."),
+    REFRESH_TOKEN_EMPTY(400, "리프레시 토큰이 존재하지 않습니다."),
+    ACCESS_TOKEN_EMPTY(401, "엑세스 토큰이 존재하지 않습니다.");
+
+    private final int status;
+    private final String message;
+}
