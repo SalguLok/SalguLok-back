ca1cc85e689c336787784327d3318a745543f14c
jettieb <bokjihee@naver.com>
2025-07-27 16:39:31 +0900

    Feat: region 및 user 엔티티 생성 #3

---
+++ salgulok/src/main/java/com/salgulok/auth/domain/User.java
@@ -0,0 +1,58 @@
+package com.salgulok.auth.domain;
+
+import com.salgulok.region.domain.Region;
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Entity
+@Getter
+@Table(name = "users")
+@EntityListeners(AuditingEntityListener.class)
+@NoArgsConstructor(access = PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long userId;
+
+    @Column(nullable = false)
+    private Long kakaoId;
+
+    private String username;
+
+    @Column(name = "profile_img")
+    private String profileImg;
+
+    private String intro;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "regionId")
+    private Region region;  // 체류 지역
+
+    @Column(name = "is_traveling", nullable = false)
+    private Boolean isTraveling = false;    // 기본값
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @Column(name = "last_login_at")
+    private LocalDateTime lastLoginAt;
+
+    @Builder
+    public User(String username, Long kakaoId) {
+        this.username = username;
+        this.kakaoId =  kakaoId;
+    }
+
+    public void updateLastLoginAt() {
+        this.lastLoginAt = LocalDateTime.now();
+    }
+}
