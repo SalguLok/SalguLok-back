2bd76f24405131c0292d7a6661ece519361dc44d
jettieb <bokjihee@naver.com>
2025-08-04 15:10:21 +0900

    Feat: 살구로그 한 달 단위 api #7

---
+++ salgulok/src/main/java/com/salgulok/log/controller/LogController.java
@@ -0,0 +1,43 @@
+package com.salgulok.log.controller;
+
+import com.salgulok.log.dto.request.LogCreateRequest;
+import com.salgulok.log.dto.request.LogUpdateRequest;
+import com.salgulok.log.dto.response.LogResponse;
+import com.salgulok.log.service.LogService;
+import com.salgulok.user.domain.User;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("/logs")
+@RequiredArgsConstructor
+public class LogController {
+    private final LogService logService;
+
+    @PostMapping
+    public ResponseEntity<Void> createLog(@AuthenticationPrincipal User user,
+                                          @Valid @RequestBody LogCreateRequest request){
+        Long logId = logService.createLog(user, request);
+        return ResponseEntity.created(URI.create("/logs/"+logId)).build();
+    }
+
+    @DeleteMapping("/{logId}")
+    public ResponseEntity<Void> deleteLog(@AuthenticationPrincipal User user,
+                                          @PathVariable Long logId){
+        logService.deleteLog(user, logId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{logId}")
+    public ResponseEntity<LogResponse> updateLog(@AuthenticationPrincipal User user,
+                                                 @PathVariable Long logId,
+                                                 @Valid @RequestBody LogUpdateRequest request){
+        LogResponse response = logService.updateLog(user, logId, request);
+        return ResponseEntity.ok(response);
+    }
+}
