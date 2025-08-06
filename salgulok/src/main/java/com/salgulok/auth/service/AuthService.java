43affd72a4d482eb585c74d73fe52401d52bafb2
jettieb <bokjihee@naver.com>
2025-08-06 18:41:11 +0900

    Feat: 로그인 시 첫 회원인 경우 구분 위해 response 수정 #8

--- salgulok/src/main/java/com/salgulok/auth/service/AuthService.java
+++ salgulok/src/main/java/com/salgulok/auth/service/AuthService.java
@@ -1,5 +1,6 @@
 package com.salgulok.auth.service;
 
+import com.salgulok.auth.dto.response.LoginResponse;
 import com.salgulok.user.domain.User;
 import com.salgulok.auth.dto.request.KakaoCodeRequest;
 import com.salgulok.auth.dto.response.JwtTokenResponse;
@@ -25,21 +26,24 @@ public class AuthService {
     private final UserRepository userRepository;
 
     @Transactional
-    public JwtTokenResponse kakaoLoginOrSignUp(KakaoCodeRequest request, HttpServletResponse response){
+    public LoginResponse kakaoLoginOrSignUp(KakaoCodeRequest request, HttpServletResponse response){
         String kakaoAccessToken = request.getCode();
         KakaoUserInfo userInfo = kakaoService.getKakaoIdFromAccessToken(kakaoAccessToken);
+
         Long kakaoId = userInfo.getKakaoId();
+        User user = userRepository.findByKakaoId(kakaoId).orElse(null);
+        boolean isNewUser = false;
 
         // 존재하는 회원이 없으면 회원 저장 후 토큰 발급
-        User user = userRepository.findByKakaoId(kakaoId)
-                .orElseGet(() -> {
-                    User newUser = User.builder()
-                            .kakaoId(kakaoId)
-                            .build();
-                    return userRepository.save(newUser);
-                });
+        if (user == null) {
+            isNewUser = true;
+            user = userRepository.save(User.builder()
+                    .kakaoId(kakaoId)
+                    .build());
+        }
 
-        return createTokens(user, response);
+        JwtTokenResponse token = createTokens(user, response);
+        return new LoginResponse(token, isNewUser);
     }
 
     private JwtTokenResponse createTokens(User user, HttpServletResponse response){
