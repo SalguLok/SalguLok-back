bcfa6198375cbd319876a8c707218d14b82cab47
김문원 <junexmun@gimmun-won-ui-MacBookAir.local>
2025-08-06 19:21:25 +0900

    places folder added

--- salgulok/src/main/java/com/salgulok/region/repository/RegionRepository.java
+++
@@ -1,7 +0,0 @@
-package com.salgulok.region.repository;
-
-import com.salgulok.region.domain.Region;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RegionRepository extends JpaRepository<Region, Long> {
-}
