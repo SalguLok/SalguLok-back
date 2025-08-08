6eca1af8023709ad10fb865be844084bf4581b5b
junexmun <angkmfirefoxygal@sookmyung.ac.kr>
2025-08-08 12:40:33 +0900

    feat: 위치 기반 및 키워드 기반 api 호출하여 json 리스트 받기. erd 수정 + repository 수정 필요

--- salgulok/src/main/java/com/salgulok/global/config/TourWebClientConfig.java
+++ salgulok/src/main/java/com/salgulok/global/config/TourWebClientConfig.java
@@ -1,21 +1,20 @@
 package com.salgulok.global.config;
 
-
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
-
+import org.springframework.web.util.DefaultUriBuilderFactory;
 
 @Configuration
 public class TourWebClientConfig {
-    @Value("${tourapi.url}")
-    private String tourApiUrl;
 
     @Bean
     public WebClient tourApiWebClient() {
+        DefaultUriBuilderFactory f = new DefaultUriBuilderFactory("https://apis.data.go.kr");
+        f.setEncodingMode(DefaultUriBuilderFactory.EncodingMode.VALUES_ONLY); // ← 중요
+
         return WebClient.builder()
-                .baseUrl(tourApiUrl)  // 환경변수에서 주입된 Base URL
+                .baseUrl("https://apis.data.go.kr")
                 .build();
     }
 }
--- salgulok/src/main/java/com/salgulok/tourapi/dto/LocationTourDto.java
+++ salgulok/src/main/java/com/salgulok/tourapi/dto/LocationTourDto.java
@@ -8,10 +8,16 @@ import lombok.*;
 @AllArgsConstructor
 public class LocationTourDto {
     private String title;       // 관광지명
-    private String address;     // 주소
+    private String address;     // 주
     private String tel;         // 전화번호
     private double mapX;        // 경도
     private double mapY;        // 위도
     private String contentType; // 콘텐츠 유형 (관광지/숙박/음식 등)
     private double distance;    // 거리 (m)
+    // 추가
+    private Long contentId;         // contentid
+    private Integer contentTypeId;  // contenttypeid 원본
+    private String imageUrl1;       // firstimage
+    private String imageUrl2;       // firstimage2
+    private String copyrightCode;   // cpyrhtDivCd
 }
