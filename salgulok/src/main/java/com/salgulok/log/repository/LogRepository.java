673e93b85078deb6375f67bb871b2b1ec4b6ab98
KKANGCHONG <leee0414@naver.com>
2025-09-10 15:30:47 +0900

    feat(log): 조회 기능 및 조회수 증가 로직 추가
    - LogService에 공개/사용자별/상세 조회 메서드 구현
    - LogRepository에 조회용 쿼리 메서드 추가
    - LogController에 조회 관련 엔드포인트(/public, /me, /users/{userId}, /{logId}, /{logId}/views) 추가
    - Log 엔티티에 조회수 증가 메서드(increaseView) 추가

--- salgulok/src/main/java/com/salgulok/log/repository/LogRepository.java
+++ salgulok/src/main/java/com/salgulok/log/repository/LogRepository.java
@@ -2,6 +2,17 @@ package com.salgulok.log.repository;
 
 import com.salgulok.log.domain.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LogRepository extends JpaRepository<Log, Long> {
+    // 전체 공개 살구록 리스트
+    List<Log> findByIsPublicTrue();
+
+    // 특정 유저 살구록 리스트
+    List<Log> findByUser_UserId(Long userId);
+
+    // 내 살구록 리스트 (컨트롤러 단에서 userId 주입하면 위 메서드 재사용)
 }
