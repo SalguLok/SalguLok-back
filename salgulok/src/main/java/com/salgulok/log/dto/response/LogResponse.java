350bd3d73039d1ea68017c9bec9e2cf1f89ceccd
KKANGCHONG <leee0414@naver.com>
2025-09-18 19:33:02 +0900

    isUpload 기능 추가

--- salgulok/src/main/java/com/salgulok/log/dto/response/LogResponse.java
+++ salgulok/src/main/java/com/salgulok/log/dto/response/LogResponse.java
@@ -18,6 +18,7 @@ public class LogResponse {
     private String imgUrl;
     private String oneReview;
     private Long likes;
+    private Boolean isUpload;
 
     public static LogResponse from(Log log){
         return LogResponse.builder()
@@ -30,7 +31,7 @@ public class LogResponse {
                 .imgUrl(log.getImgUrl())
                 .oneReview(log.getOneReview())
                 .likes(log.getLikes())
-
+                .isUpload(log.getIsUpload())
                 .build();
     }
 }
