9c8a818ca46446b3745a7adda0fd79081a488f88
junexmun <angkmfirefoxygal@sookmyung.ac.kr>
2025-09-25 22:15:00 +0900

    feat: comment 이미지 반환

--- salgulok/src/main/java/com/salgulok/log/dto/response/LogCommentResponse.java
+++ salgulok/src/main/java/com/salgulok/log/dto/response/LogCommentResponse.java
@@ -12,6 +12,7 @@ public class LogCommentResponse {
     private Long logId;
     private Long authorId;
     private String authorName;
+    private String authorProfileImg;
     private String content;
     private LocalDateTime createdAt;
 
@@ -20,6 +21,7 @@ public class LogCommentResponse {
         this.logId = logComment.getLog().getLogId();
         this.authorId = logComment.getAuthor().getUserId();
         this.authorName = logComment.getAuthor().getUsername();
+        this.authorProfileImg = logComment.getAuthor().getProfileImg();
         this.content = logComment.getContent();
         this.createdAt = logComment.getCreatedAt();
     }
